fbb30fd73d8428bd21a315c686adfb6193c738f0
Ryan S. Brown <sb@ryansb.com>
2014-03-10 09:49:49 -0500

    update test

--- persist/basic_test.go
+++ persist/basic_test.go
@@ -1,10 +1,12 @@
 package persist
 
 import (
+	"encoding/json"
 	"github.com/HouzuoGuo/tiedot/tdlog"
 	"github.com/ryansb/legowebservices/persist/kv"
 	. "github.com/ryansb/legowebservices/util/m"
 	. "launchpad.net/gocheck"
+	"os"
 	"testing"
 )
 
@@ -18,9 +20,50 @@ type TS struct{}
 
 var _ = Suite(&TS{})
 
+func (s *TS) TearDownTest(c *C) {
+	c.Check(os.RemoveAll("./tmp"), Equals, nil)
+}
+
+func getEngine() *kv.TiedotEngine {
+	return kv.NewTiedotEngine("./tmp", []string{"fake"}, kv.DropIfExist)
+}
+
 func (s *TS) TestNewCollection(c *C) {
-	engine := kv.NewTiedotEngine("./tmp", []string{"fake", "anotherfake"}, kv.KeepIfExist)
+	engine := getEngine()
+	defer engine.DB().Close()
 
 	err := engine.Insert("fake", M{"name": "Bob", "age": 42})
 	c.Check(err, Equals, nil)
+}
+
+type person struct {
+	Name string
+	Age  int
+}
+
+func (p person) ToM() M {
+	return M{"Name": p.Name, "Age": p.Age}
+}
+
+func (s *TS) TestSimpleGet(c *C) {
+	engine := getEngine()
+	defer engine.DB().Close()
+	engine.AddIndex("fake", kv.Path{"Name"})
+
+	err := engine.Insert("fake", person{Name: "Bob", Age: 42})
+	c.Assert(err, Equals, nil)
+
+	res, err := engine.Query("fake").Equals(kv.Path{"Name"}, "Bob").All()
+	c.Check(err, Equals, nil)
+
+	c.Assert(len(res), Equals, 1)
+	for _, v := range res {
+		m, err := json.Marshal(v)
+		c.Assert(err, Equals, nil)
+		bob := person{}
+		err = json.Unmarshal(m, &bob)
+		c.Assert(err, Equals, nil)
+		c.Check(bob.Name, Equals, "Bob")
+		c.Check(bob.Age, Equals, 42)
+	}
 }
